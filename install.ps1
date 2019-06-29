4da53954ea8b33d472317dee158549665893e7b0
SealabJaster <sealabjaster@gmail.com>
2019-06-29 20:01:25 +0100

    Misc

--- install.ps1
+++ install.ps1
@@ -2,6 +2,8 @@ if ([System.Environment]::OSVersion.Platform -ne [System.PlatformID]::Unix) {
     throw "Only UNIX is supported right now. Certbot, pls come to windows ;(";
 }
 
+Write-Host "### NOTE: If it looks like compliation freezes, just wait. It's probably due to low memory. ###"
+
 $CWD = $(Get-Location)
 $OutputFile = [System.IO.Path]::Combine($CWD, "bin", "aim")
 
