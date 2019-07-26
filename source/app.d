5c0e3c9a6c4d4eeadb9c855e9a95b2bcb3e0349c
SealabJaster <sealabjaster@gmail.com>
2019-07-26 23:06:39 +0100

    Add prelim verbose support

--- source/app.d
+++ source/app.d
@@ -4,6 +4,8 @@ import aim.secrets, aim.deploy.commands;
 
 int main(string[] args)
 {
+	Shell.useVerboseOutput = true; // Until we have proper support for it.
+
 	auto core = new CommandLineInterface!(
 		aim.secrets.commands,
 		aim.deploy.commands
